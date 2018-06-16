d942bfd279ec8738ef09396985b28fd05868bbf1
SeraphJACK <seraphjack@outlook.com>
2018-06-17 03:46:38 +0800

    Still Statistics....

--- src/test/java/team/covertdragon/duanwu/TestStatistics.java
+++ src/test/java/team/covertdragon/duanwu/TestStatistics.java
@@ -3,27 +3,26 @@ package team.covertdragon.duanwu;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-public class testStatistics {
+public class TestStatistics {
 
     @Test
     public void testVote() throws Throwable {
-        URL url = new URL("http://localhost:8080");
+        URL url = new URL("http://duanwu.tritusk.info");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setDoOutput(true);
         IOUtils.write("flavor=salty", connection.getOutputStream(), StandardCharsets.UTF_8);
-        InputStream is = connection.getInputStream();
+        connection.getInputStream();
         connection.disconnect();
     }
 
     @Test
     public void testQueryStatistics() throws Throwable {
-        URL url = new URL("http://localhost:8080");
+        URL url = new URL("http://duanwu.tritusk.info");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         String json = IOUtils.readLines(connection.getInputStream(), StandardCharsets.UTF_8).get(0);
         connection.disconnect();
