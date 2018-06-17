ec72d190f82c2b6d597a72889cd6a403af1f79fb
3TUSK <urey.s.knowledge@gmail.com>
2018-06-16 19:09:36 -0700

    @SeraphJACK's fault, we are kickstarting

---
+++ src/main/java/team/covertdragon/duanwu/Duanwu.java
@@ -0,0 +1,13 @@
+package team.covertdragon.duanwu;
+
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "duanwu", name = "Duanwu", version = "@VERSION_INJECT@", useMetadata = true)
+public final class Duanwu {
+
+    public Duanwu() {
+        FluidRegistry.enableUniversalBucket();
+    }
+
+}
